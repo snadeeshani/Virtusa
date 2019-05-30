433dc61029a5bc939de6e3a7ecfbf5a0063bed8f
snadeeshani <39910678+snadeeshani@users.noreply.github.com>
2019-05-30 12:06:09 +0530

    Add files via upload

---
+++ Auth-Server/src/main/java/com/codelab/auth/confi/AuthServerConfiguration.java
@@ -0,0 +1,53 @@
+package com.codelab.auth.confi;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+public class AuthServerConfiguration  extends WebSecurityConfigurerAdapter implements AuthorizationServerConfigurer{
+
+	PasswordEncoder passwordEncoder=PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception{
+		return super.authenticationManager();
+	}
+	@Autowired
+	AuthenticationManager authonticationMAnager;
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.checkTokenAccess("permitAll");
+		
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("web").secret(passwordEncoder.encode("web"))
+		.authorizedGrantTypes("password").scopes("mobile").accessTokenValiditySeconds(3600);
+		
+		
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authonticationMAnager)
+		.allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+		
+	}
+
+	
+	
+	
+}
---
+++ Auth-Server/src/main/java/com/codelab/auth/confi/UserConfiguration.java
@@ -0,0 +1,29 @@
+package com.codelab.auth.confi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+	PasswordEncoder passwordEncoder=PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	
+	@Override
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("Nadeeshani")
+		.password(passwordEncoder.encode("Nadeeshani"))
+		.roles("ADMIN","USER")
+		.authorities("CAN_CREATE","CAN_READ")
+		.and()
+		.withUser("Saman")
+		.password(passwordEncoder.encode("saman"))
+				.roles("ADMIN","USER")
+				.authorities("CAN_READ");;
+	}
+	
+	
+
+}
